9a8c6dc2a9936372ab90927de4aeac45932bda7d
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-31 21:53:49 +0300

    1.

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FiyatTarifeForm.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FiyatTarifeForm.Designer.cs
@@ -31,6 +31,7 @@ namespace OtoparkOtomasyonuEnSon
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FiyatTarifeForm));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -50,6 +51,7 @@ namespace OtoparkOtomasyonuEnSon
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FiyatTarifeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FiyatTarifeForm";
