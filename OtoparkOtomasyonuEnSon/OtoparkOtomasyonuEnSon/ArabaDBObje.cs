0eba5fb4e48a983fee763751e2c1a758268c0b3d
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-31 22:42:32 +0300

    bunu bi güncelle

---
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/ArabaDBObje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OtoparkOtomasyonuEnSon
+{
+    public class ArabaDBObje
+    {
+        public string Plaka;
+        public string TelefonNumarasi;
+        public DateTime GirisSaati;
+        public DateTime CikisSaati;
+        public float Tutar;
+
+        public ArabaDBObje(string plaka, string telefonNumarasi, DateTime girisSaati)
+        {
+            Plaka = plaka;
+            TelefonNumarasi = telefonNumarasi;
+            GirisSaati = girisSaati;
+        }
+    }
+}
