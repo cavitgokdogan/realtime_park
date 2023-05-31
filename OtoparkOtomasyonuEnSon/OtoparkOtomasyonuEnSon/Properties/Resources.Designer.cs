935862ab4ac20f2fede7b80f23ab0c2177cb7c76
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-31 21:41:32 +0300

    referansları kaldırmamışım bi sn

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
@@ -103,26 +103,6 @@ namespace OtoparkOtomasyonuEnSon.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap parklogo {
-            get {
-                object obj = ResourceManager.GetObject("parklogo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap parklogo2 {
-            get {
-                object obj = ResourceManager.GetObject("parklogo2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Splash {
             get {
                 object obj = ResourceManager.GetObject("Splash", resourceCulture);
