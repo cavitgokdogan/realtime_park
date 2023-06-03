c9167481397ae62bf627fc00ea7078e78979609a
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-03 22:15:20 +0300

    eski otomatik giriş şeyini kaldırıyorum
    Co-Authored-By: EmreSafaCelik <69720413+EmreSafaCelik@users.noreply.github.com>

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.cs
@@ -28,56 +28,6 @@ namespace OtoparkOtomasyonuEnSon
             //Manuel Giriş Sayfasına Geçiş
         }
 
-        private void automaticEntranceButton_Click(object sender, EventArgs e)
-        {
-            // Start a new process
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "python",
-                    // Replace with your python script path
-                    Arguments = "..\\..\\..\\..\\plaka_tanima\\cam_plaka.py",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory
-                }
-            };
-            process.Start();
-
-            // Read the python script output
-            while (!process.StandardOutput.EndOfStream)
-            {
-                var line = process.StandardOutput.ReadLine();
-                if (!string.IsNullOrEmpty(line))
-                {
-                    // Store the detected text
-                    string detectedText = line;
-                    MessageBox.Show(detectedText + " plakalı cihaz kaydedildi");
-                    try
-                    {
-                        using (SqlConnection connection = new SqlConnection(ConnectionString))
-                        {
-                            connection.Open();
-                            string insertArabalar = "insert into dbo.arabalar (plaka, telefon_no,giris_saati) values (@p1,@p2,@p3)";
-                            using (SqlCommand command = new SqlCommand(insertArabalar, connection))
-                            {
-                                command.Parameters.AddWithValue("@p1", detectedText);
-                                command.Parameters.AddWithValue("@p2", "-");
-                                command.Parameters.AddWithValue("@p3", DateTime.Now);
-                                command.ExecuteNonQuery();
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Bağlantı Kurulamadı !\n" + ex.Message);
-                    }
-                }
-            }
-        }
-
         private void registriesButton_Click(object sender, EventArgs e)
         {
             FormKayitlar formKayitlar = new FormKayitlar();
