61e590354575043124f302bf1c258919cc3b4c9b
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-03 22:20:53 +0300

    2/3

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.cs
@@ -158,6 +158,8 @@ namespace OtoparkOtomasyonuEnSon
         }
 
         private void PnlOtomatikGiris_Click(object sender, EventArgs e) => OtomatikGiris();
+
+        private void LblOtomatikGiris_Click(object sender, EventArgs e) => OtomatikGiris();
         #endregion
 
         #region Eyecandy Sağlayan Metotlar ve Olaylar
