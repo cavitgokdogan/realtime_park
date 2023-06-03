958344f7edd0ba67ab3b51cfc28d0c6154dbb906
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-03 22:43:42 +0300

    yeni ikon

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace OtoparkOtomasyonuEnSon.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_admin {
+            get {
+                object obj = ResourceManager.GetObject("icon-admin", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon icon_app {
