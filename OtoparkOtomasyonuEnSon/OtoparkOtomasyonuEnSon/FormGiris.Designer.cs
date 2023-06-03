0225184fc03bac1f40e2d06ad2df8735e007b6a8
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-03 21:07:29 +0300

    tatlış yeni elemanlar

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormGiris.Designer.cs
@@ -34,16 +34,20 @@ namespace OtoparkOtomasyonuEnSon
             this.picCikisYap = new System.Windows.Forms.PictureBox();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.stripSaat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panCikisYap.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picCikisYap)).BeginInit();
             this.statusStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panCikisYap
             // 
             this.panCikisYap.Controls.Add(this.lblCikisYap);
             this.panCikisYap.Controls.Add(this.picCikisYap);
-            this.panCikisYap.Location = new System.Drawing.Point(12, 12);
+            this.panCikisYap.Location = new System.Drawing.Point(6, 12);
             this.panCikisYap.Name = "panCikisYap";
             this.panCikisYap.Size = new System.Drawing.Size(133, 56);
             this.panCikisYap.TabIndex = 7;
@@ -95,14 +99,47 @@ namespace OtoparkOtomasyonuEnSon
             this.stripSaat.Size = new System.Drawing.Size(84, 24);
             this.stripSaat.Text = "Saat: 12.00";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.panCikisYap);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 76);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(528, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Otopark Otomasyon Sistemi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(651, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "versiyon 2023.01";
+            // 
             // FormGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(33)))), ((int)(((byte)(184)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.statusStrip);
-            this.Controls.Add(this.panCikisYap);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormGiris";
@@ -114,6 +151,8 @@ namespace OtoparkOtomasyonuEnSon
             ((System.ComponentModel.ISupportInitialize)(this.picCikisYap)).EndInit();
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,5 +165,9 @@ namespace OtoparkOtomasyonuEnSon
         private System.Windows.Forms.ToolStripStatusLabel stripSaat;
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
