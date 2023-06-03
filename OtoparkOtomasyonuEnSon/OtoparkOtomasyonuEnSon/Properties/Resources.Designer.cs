fae227d66a8fef6d9064eef0882e0d0402fc6358
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-03 21:11:26 +0300

    yeni ikon: fiyat

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace OtoparkOtomasyonuEnSon.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_price {
+            get {
+                object obj = ResourceManager.GetObject("icon_price", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon_user {
             get {
                 object obj = ResourceManager.GetObject("icon_user", resourceCulture);
