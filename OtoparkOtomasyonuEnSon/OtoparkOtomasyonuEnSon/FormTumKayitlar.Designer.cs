7dca48cf306e7017a68dab50dffbc91c6ac4e7cb
Altay Turan <69851770+altayturan@users.noreply.github.com>
2023-06-04 19:31:13 +0300

    feat: TumKayitlar

---
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormTumKayitlar.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace OtoparkOtomasyonuEnSon
+{
+    partial class FormTumKayitlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.registryGridView = new System.Windows.Forms.DataGridView();
+            this.otoparkDataSet = new OtoparkOtomasyonuEnSon.otoparkDataSet();
+            this.arabalarBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.arabalarTableAdapter = new OtoparkOtomasyonuEnSon.otoparkDataSetTableAdapters.arabalarTableAdapter();
+            this.plakaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefonnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.girissaatiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cikissaatiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tutarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnlCikisYap = new System.Windows.Forms.Panel();
+            this.lblCikisYap = new System.Windows.Forms.Label();
+            this.picCikisYap = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.registryGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.otoparkDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arabalarBindingSource)).BeginInit();
+            this.pnlCikisYap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCikisYap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // registryGridView
+            // 
+            this.registryGridView.AllowUserToAddRows = false;
+            this.registryGridView.AllowUserToDeleteRows = false;
+            this.registryGridView.AllowUserToOrderColumns = true;
+            this.registryGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.registryGridView.AutoGenerateColumns = false;
+            this.registryGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(42)))), ((int)(((byte)(235)))));
+            this.registryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.registryGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.plakaDataGridViewTextBoxColumn,
+            this.telefonnoDataGridViewTextBoxColumn,
+            this.girissaatiDataGridViewTextBoxColumn,
+            this.cikissaatiDataGridViewTextBoxColumn,
+            this.tutarDataGridViewTextBoxColumn});
+            this.registryGridView.DataSource = this.arabalarBindingSource;
+            this.registryGridView.Location = new System.Drawing.Point(191, 26);
+            this.registryGridView.Margin = new System.Windows.Forms.Padding(4);
+            this.registryGridView.Name = "registryGridView";
+            this.registryGridView.ReadOnly = true;
+            this.registryGridView.RowHeadersWidth = 51;
+            this.registryGridView.Size = new System.Drawing.Size(554, 384);
+            this.registryGridView.TabIndex = 1;
+            // 
+            // otoparkDataSet
+            // 
+            this.otoparkDataSet.DataSetName = "otoparkDataSet";
+            this.otoparkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // arabalarBindingSource
+            // 
+            this.arabalarBindingSource.DataMember = "arabalar";
+            this.arabalarBindingSource.DataSource = this.otoparkDataSet;
+            // 
+            // arabalarTableAdapter
+            // 
+            this.arabalarTableAdapter.ClearBeforeFill = true;
+            // 
+            // plakaDataGridViewTextBoxColumn
+            // 
+            this.plakaDataGridViewTextBoxColumn.DataPropertyName = "plaka";
+            this.plakaDataGridViewTextBoxColumn.HeaderText = "plaka";
+            this.plakaDataGridViewTextBoxColumn.Name = "plakaDataGridViewTextBoxColumn";
+            this.plakaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // telefonnoDataGridViewTextBoxColumn
+            // 
+            this.telefonnoDataGridViewTextBoxColumn.DataPropertyName = "telefon_no";
+            this.telefonnoDataGridViewTextBoxColumn.HeaderText = "telefon_no";
+            this.telefonnoDataGridViewTextBoxColumn.Name = "telefonnoDataGridViewTextBoxColumn";
+            this.telefonnoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // girissaatiDataGridViewTextBoxColumn
+            // 
+            this.girissaatiDataGridViewTextBoxColumn.DataPropertyName = "giris_saati";
+            this.girissaatiDataGridViewTextBoxColumn.HeaderText = "giris_saati";
+            this.girissaatiDataGridViewTextBoxColumn.Name = "girissaatiDataGridViewTextBoxColumn";
+            this.girissaatiDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // cikissaatiDataGridViewTextBoxColumn
+            // 
+            this.cikissaatiDataGridViewTextBoxColumn.DataPropertyName = "cikis_saati";
+            this.cikissaatiDataGridViewTextBoxColumn.HeaderText = "cikis_saati";
+            this.cikissaatiDataGridViewTextBoxColumn.Name = "cikissaatiDataGridViewTextBoxColumn";
+            this.cikissaatiDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // tutarDataGridViewTextBoxColumn
+            // 
+            this.tutarDataGridViewTextBoxColumn.DataPropertyName = "tutar";
+            this.tutarDataGridViewTextBoxColumn.HeaderText = "tutar";
+            this.tutarDataGridViewTextBoxColumn.Name = "tutarDataGridViewTextBoxColumn";
+            this.tutarDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // pnlCikisYap
+            // 
+            this.pnlCikisYap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlCikisYap.Controls.Add(this.lblCikisYap);
+            this.pnlCikisYap.Controls.Add(this.picCikisYap);
+            this.pnlCikisYap.Location = new System.Drawing.Point(13, 352);
+            this.pnlCikisYap.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlCikisYap.Name = "pnlCikisYap";
+            this.pnlCikisYap.Size = new System.Drawing.Size(159, 58);
+            this.pnlCikisYap.TabIndex = 14;
+            // 
+            // lblCikisYap
+            // 
+            this.lblCikisYap.AutoSize = true;
+            this.lblCikisYap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCikisYap.ForeColor = System.Drawing.Color.White;
+            this.lblCikisYap.Location = new System.Drawing.Point(59, 18);
+            this.lblCikisYap.Name = "lblCikisYap";
+            this.lblCikisYap.Size = new System.Drawing.Size(68, 18);
+            this.lblCikisYap.TabIndex = 1;
+            this.lblCikisYap.Text = "Geri Dön";
+            // 
+            // picCikisYap
+            // 
+            this.picCikisYap.Image = global::OtoparkOtomasyonuEnSon.Properties.Resources.icon_logout;
+            this.picCikisYap.Location = new System.Drawing.Point(3, 3);
+            this.picCikisYap.Name = "picCikisYap";
+            this.picCikisYap.Size = new System.Drawing.Size(50, 50);
+            this.picCikisYap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCikisYap.TabIndex = 0;
+            this.picCikisYap.TabStop = false;
+            // 
+            // FormTumKayitlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(33)))), ((int)(((byte)(184)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlCikisYap);
+            this.Controls.Add(this.registryGridView);
+            this.Name = "FormTumKayitlar";
+            this.Text = "FormTumKayitlar";
+            this.Load += new System.EventHandler(this.FormTumKayitlar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.registryGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.otoparkDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arabalarBindingSource)).EndInit();
+            this.pnlCikisYap.ResumeLayout(false);
+            this.pnlCikisYap.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCikisYap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView registryGridView;
+        private otoparkDataSet otoparkDataSet;
+        private System.Windows.Forms.BindingSource arabalarBindingSource;
+        private otoparkDataSetTableAdapters.arabalarTableAdapter arabalarTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn plakaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefonnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn girissaatiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cikissaatiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tutarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Panel pnlCikisYap;
+        private System.Windows.Forms.Label lblCikisYap;
+        private System.Windows.Forms.PictureBox picCikisYap;
+    }
+}
