5883e5249288a9a4c7e04dc5d778a7c4f48f77cf
EmreSafaCelik <safaemrecelik@gmail.com>
2023-06-06 12:07:27 +0300

    Update FormManual.Designer.cs

--- OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormManual.Designer.cs
+++ OtoparkOtomasyonuEnSon/OtoparkOtomasyonuEnSon/FormManual.Designer.cs
@@ -288,6 +288,7 @@
             // 
             // __label2
             // 
+            this.@__label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.@__label2.AutoSize = true;
             this.@__label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.@__label2.ForeColor = System.Drawing.Color.White;
